386f87ee6fdf96a22856c66ad50cee229929171b
LaiTeng <tengpang0208@gmail.com>
2020-07-30 16:23:24 +0800

    create html file

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/") # Revisit decorators if you unclear of this syntax
 def index():
-    return '<h1>Why so easy</h1>'
+    return render_template('index.html')
 
 # @app.route("/another")
 # def show():
@@ -11,7 +11,7 @@ def index():
 
 @app.route('/user/<username>')
 def show(username):
-    return f"Hi {username[0:3]}"
+    return f"Hi {username[5]}"
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
-   app.run()
+   app.run(debug=True)
